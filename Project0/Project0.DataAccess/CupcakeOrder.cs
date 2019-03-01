cb0ba516a95a3870e2970a6d0a22b1117c2261d7
Your Name <you@example.com>
2019-03-01 09:09:55 -0600

    Took the old Project0 methods, added them to the new Project0 with ORM, and commented them all out

--- Project0/Project0.DataAccess/CupcakeOrder.cs
+++ Project0/Project0.DataAccess/CupcakeOrder.cs
@@ -15,5 +15,86 @@ namespace Project0.DataAccess
         public virtual Cupcake Cupcake { get; set; }
         public virtual Customer Customer { get; set; }
         public virtual Location Location { get; set; }
+
+        //public static int AddOrder(int storeLocationId,
+        //                            int customerId,
+        //                            CupcakeNum cupcakeType,
+        //                            Cupcake lookupCupcake,
+        //                            int qnty,
+        //                            string jsonLocations,
+        //                            string jsonCustomers,
+        //                            string jsonOrders,
+        //                            List<Customer> customers,
+        //                            List<Location> storeLocations,
+        //                            List<Order> orders)
+        //{
+        //    foreach (var item in storeLocations.Where(sL => sL.Id == storeLocationId))
+        //    {
+        //        item.UpdateInv(lookupCupcake, qnty);
+        //    }
+
+        //    int newOrderId = 1;
+        //    if (orders.Count > 0) { newOrderId = orders.Max(o => o.Id) + 1; }
+
+        //    Order newOrder = new Order
+        //    {
+        //        Id = newOrderId,
+        //        OrderLocation = storeLocationId,
+        //        OrderCustomer = customerId,
+        //        OrderTime = DateTime.Now,
+        //        OrderItem = (lookupCupcake, qnty, cupcakeType)
+        //    };
+
+        //    orders.Add(newOrder);
+        //    string newData = JsonConvert.SerializeObject(orders, Formatting.Indented);
+        //    File.WriteAllTextAsync(jsonOrders, newData).Wait();
+
+        //    // https://stackoverflow.com/questions/19930450/conditional-updating-a-list-using-linq
+        //    foreach (var item in storeLocations.Where(sL => sL.Id == storeLocationId))
+        //    {
+        //        item.OrderHistory.Add(newOrder);
+        //    }
+        //    string newData2 = JsonConvert.SerializeObject(storeLocations, Formatting.Indented);
+        //    File.WriteAllTextAsync(jsonLocations, newData2).Wait();
+
+        //    foreach (var item in customers.Where(c => c.Id == customerId))
+        //    {
+        //        item.OrderHistory.Add(newOrder);
+        //        item.LastOrder = DateTime.Now;
+        //        item.LastStoreOrder = storeLocationId;
+        //    }
+        //    string newData3 = JsonConvert.SerializeObject(customers, Formatting.Indented);
+        //    File.WriteAllTextAsync(jsonCustomers, newData3).Wait();
+        //    return newOrderId;
+        //}
+
+        //public static bool CheckOrderFeasible(int storeLocationId, List<Location> storeLocations,
+        //    Cupcake lookupCupcake, int qnty)
+        //{
+        //    bool orderFeasible = false;
+        //    // Check store inventory to make sure there are enough ingredients
+        //    foreach (var item in storeLocations.Where(sL => sL.Id == storeLocationId))
+        //    {
+        //        orderFeasible = item.CheckInv(lookupCupcake, qnty);
+        //    }
+        //    return orderFeasible;
+        //}
+
+        //public static bool CheckCustomerCanOrder(int customerId, int storeLocationId, List<Customer> customers)
+        //{
+        //    bool customerCanOrder = true;
+        //    foreach (var item in customers.Where(c => c.Id == customerId))
+        //    {
+        //        DateTime currentTime = DateTime.Now;
+        //        if ((Math.Abs((item.LastOrder).Subtract(currentTime).TotalMinutes) < 120) &&
+        //                item.LastStoreOrder == storeLocationId)
+        //        {
+        //            customerCanOrder = false;
+        //        }
+        //    }
+        //    return customerCanOrder;
+        //}
     }
 }
+
+   
--- Project0/Project0/Program.cs
+++ Project0/Project0/Program.cs
@@ -1,15 +1,213 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace Project0
 {
     class Program
     {
-        // 5. run command:
-        //      Scaffold-DbContext "<your-connection-string>" 
-        //      Microsoft.EntityFrameworkCore.SqlServer
-        //      -Project <name-of-data-project>
         static void Main(string[] args)
         {
+            ILogger logger = LogManager.GetCurrentClassLogger();
+
+            //    string jsonLocations = @"C:\revature\luigi_cupcakes_locations.json";
+            //    string jsonCustomers = @"C:\revature\luigi_cupcakes_customers.json";
+            //    string jsonOrders = @"C:\revature\luigi_cupcakes_orders.json";
+            //    LoadDataFromJSON(jsonLocations,
+            //                    jsonCustomers,
+            //                    jsonOrders,
+            //                    out var storeLocations,
+            //                    out var customers,
+            //                    out var orders);
+
+            Console.WriteLine("Welcome to Luigi's Cupcakes Manager");
+            Console.WriteLine();
+            Console.WriteLine("Please select from the following options (not case-sensitive):");
+
+            //    while (true)
+            //    {
+            //        ConsoleDisplay.DisplayMenu();
+            //        ConsoleRead.GetMenuInput(out var input);
+            //        if (input == "S")
+            //        {
+            //            GetDataAndAddLocation(jsonLocations, storeLocations);
+            //        }
+            //        else if (input == "C")
+            //        {
+            //            GetDataAndAddCustomer(jsonCustomers, customers, storeLocations);
+            //        }
+            //        else if (input == "O")
+            //        {
+            //            GetDataAndAddOrder(jsonLocations, jsonCustomers, jsonOrders,
+            //                customers, storeLocations, orders);
+            //        }
+            //        else if (input == "SL")
+            //        {
+            //            ConsoleDisplay.StoreList(storeLocations);
+            //        }
+            //        else if (input == "SO")
+            //        {
+            //            ConsoleRead.StoreOrders(storeLocations);
+            //        }
+            //        else if (input == "CL")
+            //        {
+            //            ConsoleDisplay.CustomerList(customers);
+            //        }
+            //        else if (input == "CS")
+            //        {
+            //            ConsoleRead.CustomerSearch(customers);
+            //        }
+            //        else if (input == "CO")
+            //        {
+            //            ConsoleRead.CustomerOrders(customers);
+            //        }
+            //        else if (input == "OL")
+            //        {
+            //            ConsoleDisplay.OrderList(orders, storeLocations);
+            //        }
+            //        else if (input == "OR")
+            //        {
+            //            ConsoleRead.OrderRecommended(customers, orders);
+            //        }
+            //        else if (input == "Q")
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //public static void LoadDataFromJSON(string jsonLocations,
+            //                                    string jsonCustomers,
+            //                                    string jsonOrders,
+            //                                    out List<Location> storeLocations,
+            //                                    out List<Customer> customers,
+            //                                    out List<Order> orders)
+            //{
+            //    storeLocations = new List<Location>();
+            //    if (File.Exists(jsonLocations))
+            //    {
+            //        string dataLocations = File.ReadAllTextAsync(jsonLocations).Result;
+            //        storeLocations = JsonConvert.DeserializeObject<List<Location>>(dataLocations);
+            //    }
+
+            //    customers = new List<Customer>();
+            //    if (File.Exists(jsonCustomers))
+            //    {
+            //        string dataCustomers = File.ReadAllTextAsync(jsonCustomers).Result;
+            //        customers = JsonConvert.DeserializeObject<List<Customer>>(dataCustomers);
+            //    }
+
+            //    orders = new List<Order>();
+            //    if (File.Exists(jsonOrders))
+            //    {
+            //        string dataOrders = File.ReadAllTextAsync(jsonOrders).Result;
+            //        orders = JsonConvert.DeserializeObject<List<Order>>(dataOrders);
+            //    }
+            //}
+
+            //public static void GetDataAndAddLocation(string jsonLocations, List<Location> storeLocations)
+            //{
+            //    int newLocationId = Location.AddStoreLocation(jsonLocations, storeLocations);
+            //    Console.WriteLine($"Location with id of {newLocationId} successfully created!");
+            //}
+
+            //public static void GetDataAndAddCustomer(string jsonCustomers, List<Customer> customers,
+            //    List<Location> storeLocations)
+            //{
+            //    string fName = ConsoleRead.GetCustomerFirstName();
+            //    if (fName is null) { return; }
+            //    string lName = ConsoleRead.GetCustomerLastName();
+            //    if (lName is null) { return; }
+            //    bool isStores = Location.CheckForStores(storeLocations);
+            //    if (!isStores)
+            //    {
+            //        Console.WriteLine("You must add at least one store before you can add a customer.");
+            //        return;
+            //    }
+            //    int storeLocationId = ConsoleRead.GetStoreLocation(storeLocations,
+            //        "Please enter a valid Id for default store location:");
+            //    if (storeLocationId == -1) { return; }
+            //    bool storeExists = Location.CheckStoreExists(storeLocationId, storeLocations);
+            //    if (!storeExists) { return; }
+
+            //    int newCustomerId = Customer.AddCustomer(jsonCustomers, customers, storeLocations,
+            //        fName, lName, storeLocationId);
+            //    Console.WriteLine($"Location with id of {newCustomerId} successfully created!");
+            //}
+
+            //public static void GetDataAndAddOrder(string jsonLocations,
+            //                                        string jsonCustomers,
+            //                                        string jsonOrders,
+            //                                        List<Customer> customers,
+            //                                        List<Location> storeLocations,
+            //                                        List<Order> orders)
+            //{
+            //    ILogger logger = LogManager.GetCurrentClassLogger();
+
+            //    int storeLocationId = ConsoleRead.GetStoreLocation(storeLocations,
+            //        "Please enter a valid store Id for the order:");
+            //    if (storeLocationId == -1)
+            //    {
+            //        return;
+            //    }
+            //    bool storeExists = Location.CheckStoreExists(storeLocationId, storeLocations);
+            //    if (!storeExists)
+            //    {
+            //        logger.Error($"{storeLocationId} is not in the list of stores.");
+            //        return;
+            //    }
+            //    int customerId = ConsoleRead.GetCustomer(customers);
+            //    if (customerId == -1)
+            //    {
+            //        return;
+            //    }
+            //    bool customerExists = Customer.CheckCustomerExists(customerId, customers);
+            //    if (!customerExists)
+            //    {
+            //        logger.Error($"{customerId} is not in the list of customers.");
+            //        return;
+            //    }
+            //    Cupcake cupcakeTuple = ConsoleRead.GetCupcake();
+            //    if (cupcakeTuple.Item2 is null)
+            //    {
+            //        return;
+            //    }
+            //    int orderQnty = ConsoleRead.GetCupcakeQuantity();
+            //    if (orderQnty == -1)
+            //    {
+            //        return;
+            //    }
+            //    bool qntyAllowed = Cupcake.CheckCupcakeQuantity(orderQnty);
+            //    if (!qntyAllowed)
+            //    {
+            //        Console.WriteLine("Maximum order quantity is 500.");
+            //        return;
+            //    }
+            //    bool cupcakeAllowed = Cupcake.CheckCupcake(storeLocationId, cupcakeTuple.Item1, orders);
+            //    if (!cupcakeAllowed)
+            //    {
+            //        Console.WriteLine("This store has exhausted supply of that cupcake. Try back in 24 hours.");
+            //        return;
+            //    }
+            //    bool orderFeasible = Order.CheckOrderFeasible(storeLocationId, storeLocations,
+            //        cupcakeTuple.Item2, orderQnty);
+            //    if (!orderFeasible)
+            //    {
+            //        Console.WriteLine("This store does not have enough ingredients to place the requested order.");
+            //        return;
+            //    }
+            //    bool customerCanOrder = Order.CheckCustomerCanOrder(customerId,
+            //        storeLocationId, customers);
+            //    if (!customerCanOrder)
+            //    {
+            //        Console.WriteLine("Customer can't place an order at this store because it hasn't been 2 hours yet.");
+            //        return;
+            //    }
+
+            //    int newOrderId = Order.AddOrder(storeLocationId, customerId, cupcakeTuple.Item1, cupcakeTuple.Item2, orderQnty,
+            //        jsonLocations, jsonCustomers, jsonOrders, customers, storeLocations, orders);
+            //    Console.WriteLine($"Order with id of {newOrderId} successfully created!");
+            //}
+
 
         }
     }
